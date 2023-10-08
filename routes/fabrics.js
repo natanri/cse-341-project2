5a536afc6ef2659d1da1d3f18a2135dd563040a5
u060883469 <u060883469@byui.edu>
2023-10-07 19:17:54 -0500

    authenticate.js file was added

--- routes/fabrics.js
+++ routes/fabrics.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 const fabricsControllers = require('../controllers/fabrics');
 const validation = require('../middleware/validate');
+const isAuthenticate  = require('../middleware/authenticate')
 
 router.get('/', fabricsControllers.getAll);
 
 router.get('/:id', fabricsControllers.getSingle);
 
-router.post('/', validation.saveFabrics, fabricsControllers.createFabric);
+router.post('/', validation.saveFabrics, isAuthenticate.isAuthenticated , fabricsControllers.createFabric);
 
-router.put('/:id', validation.saveFabrics,fabricsControllers.updateFabric);
+router.put('/:id', validation.saveFabrics, isAuthenticate.isAuthenticated , fabricsControllers.updateFabric);
 
-router.delete('/:id', fabricsControllers.deleteFabric);
+router.delete('/:id', validation.saveFabrics, isAuthenticate.isAuthenticated , fabricsControllers.deleteFabric);
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 // Allow us to handles routes
+const passport = require('passport');
 const router = require('express').Router();
 
+
 router.use('/', require('./swagger'));
-
-router.get('/', (req, res) => { 
-    //#swagger.tags=['Hello World']
-    res.send("Hello World")});
-
+/*router.get('/', (req, res) => { //#swagger.tags=['Hello World']
+     res.send("Hello World")});*/
 router.use('/fabrics', require('./fabrics'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next){
+    req.logout(function(err){
+        if(err) {return next(err);}
+        res.redirect('/');
+     });
+    });
+ 
 
 module.exports = router;
